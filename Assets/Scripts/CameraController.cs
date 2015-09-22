2c6350b9eb6eb615e0c6e8c767d385206ef9b93c
Wesley <wesleyyep@hotmail.com>
2015-09-22 15:48:30 +1200

    added new files and packages

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject santa;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - santa.transform.position;
+	}
+
+    void LateUpdate() {
+        transform.position = santa.transform.position + offset;
+    }
+}
