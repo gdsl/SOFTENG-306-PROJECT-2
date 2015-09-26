1d1c979ddafed53e9516363e6d4b7c3386f93dbe
Wesley <wesleyyep@hotmail.com>
2015-09-26 22:53:01 +1200

    updated folder structure and added gamecontroller

---
+++ Assets/GameController/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject santa;
+	public GameObject cameraController;
+	public GameObject animal; //could change to a list? not sure if it works with Unity associations
+	public GameObject human; //could change to a list? not sure if it works with Unity associations
+	public GameObject room;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
