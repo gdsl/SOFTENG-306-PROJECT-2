eda615ed15a2362cf44cbc77f988e14ac85844b2
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-09-27 21:21:27 +1300

    add in cookie get taken

---
+++ Assets/Scripts/CookieScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CookieScript : MonoBehaviour {
+    private bool isPickedUp=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPickedUp)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            isPickedUp = true;
+        }
+    }
+}
