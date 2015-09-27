3f2c180f01f1cd55699d0de476155eb02448ee50
Chang Kon Han <chang_kon@hotmail.com>
2015-09-28 01:18:23 +1300

    added some personsight script for people. doesn't contain any details atm

---
+++ Assets/PersonSight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonSight : MonoBehaviour {
+
+    // Number of degrees, centred on the forward position, player can see. Default value of 110 degrees
+    public float fieldOfViewAngle = 110f;
+    public bool santaInSight;
+
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
