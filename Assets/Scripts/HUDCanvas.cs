579c2cb6af3eab66feb6fbf3b5df943301e48107
Wesley <wesleyyep@hotmail.com>
2015-09-30 23:08:12 +1300

    converted HUDCanvas to prefab and added Timer text for score

--- Assets/Scripts/HUDCanvas.cs
+++ Assets/Scripts/HUDCanvas.cs
@@ -2,16 +2,17 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class HUDCanvas : MonoBehaviour {
+
+	public Text timeText;
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		timeText.text = "Time: " + Time.timeSinceLevelLoad.ToString("0.00");
 	}
-
 }
