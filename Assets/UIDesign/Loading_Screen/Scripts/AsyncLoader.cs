5ab8c7a0923feac7d7b9198c5a3b13c294f1eb61
unknown <jkim506@aucklanduni.ac.nz>
2015-10-02 17:19:08 +1300

    made loading screen between scene transition for levels

---
+++ Assets/UIDesign/Loading_Screen/Scripts/AsyncLoader.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsyncLoader : MonoBehaviour {
+
+
+    public Texture2D emptyProgressBar; // Set this in inspector.
+    public Texture2D fullProgressBar; // Set this in inspector.
+
+    private AsyncOperation async = null; // When assigned, load is in progress.
+
+    // Use this for initialization
+    void Start () {
+        Application.LoadLevel(3);
+    }
+	
+	// Update is called once per frame
+	void Update () {    }
+
+    /*
+
+    private void SyncLoadLevel(int level)
+    {
+        async = Application.LoadLevelAsync(level);
+        Load();
+    }
+
+    IEnumerator Load()
+    {
+        yield return async;
+    }
+
+    void OnGUI()
+    {
+        if (async != null)
+        {
+            GUI.DrawTexture(new Rect(0, 0, 100, 50), emptyProgressBar);
+            GUI.DrawTexture(new Rect(0, 0, 100 * async.progress, 50), fullProgressBar);
+            GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+            GUI.Label(new Rect(0, 0, 100, 50), string.Format("{0:N0}%", async.progress * 100f));
+        }
+    }
+    */
+
+}
