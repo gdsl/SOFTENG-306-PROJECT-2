44af31c9cb7facd4540d87cfbd34ef05322db76a
Chang Kon Han <chang_kon@hotmail.com>
2015-10-03 00:02:05 +1300

    Added SleepingPersonAI and SleepingScript. Haven't tested

---
+++ Assets/Scripts/SleepingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleepingScript : MonoBehaviour {
+
+    public bool sleeping;
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        // initially true
+        sleeping = true;
+
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Sleeping", sleeping);
+	}
+}
