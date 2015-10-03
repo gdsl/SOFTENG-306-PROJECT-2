9c0cd8d055a136652af22bd24c13d7d42c505763
John <johnlaw1234@hotmail.com>
2015-10-03 20:18:35 +1300

    unlock first achievement working

--- Assets/UIDesign/Achievements/Scripts/AchievementController.cs
+++ Assets/UIDesign/Achievements/Scripts/AchievementController.cs
@@ -7,28 +7,38 @@ using System.IO;
 public class AchievementController : MonoBehaviour {
 
     private List<bool> achievement;
-    //public enum AchievementType { FIRST_LEVEL_COMPLETE, TEN_COOKIES, STAY_BELOW }
+    private GameObject[] lockedImage;
+    private GameObject[] unlockedImage;
+    
+    //constant for different achievements
     public const int FIRST_LEVEL_COMPLETE = 0;
     public const int TEN_COOKIES = 1;
     public const int STAY_BELOW = 2;
     // Use this for initialization
 	void Start () {
+        //display where the achievement data is save on your computer
+        print("saved file directory: " + Application.persistentDataPath);
+
+        //find all achievement images and store them to use later on
+        lockedImage = GameObject.FindGameObjectsWithTag("LockedAchievement");
+        unlockedImage = GameObject.FindGameObjectsWithTag("UnlockedAchievement");
+        
+        //create new achievement game data if not open existing one
         if (File.Exists(Application.persistentDataPath + "/achievement.gd"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/achievement.gd", FileMode.Open);
             achievement = (List<bool>)bf.Deserialize(file);
-            file.Close();
-
-            GameObject[] lockedImage = GameObject.FindGameObjectsWithTag("LockedAchievement");
-            GameObject[] unlockedImage = GameObject.FindGameObjectsWithTag("UnlockedAchievement");
-            
+               
+            //change the UI corresponding to the existing achievement data
             foreach (GameObject image in lockedImage)
             {
                 //game object are named in "1 Locked", "2 Locked". split the string to get the value
+                
                 string[] nameArray = image.name.Split(' ');
                 int value = int.Parse(nameArray[0]) -1;
                 image.SetActive(!achievement[value]);  
+
             }
 
             foreach (GameObject image in unlockedImage)
@@ -36,8 +46,10 @@ public class AchievementController : MonoBehaviour {
                 //game object are named in "1 Locked", "2 Locked". split the string to get the value
                 string[] nameArray = image.name.Split(' ');
                 int value = int.Parse(nameArray[0]) - 1;
-                image.SetActive(!achievement[value]);
+                image.SetActive(achievement[value]);
+               
             }
+            file.Close();
         }
         else {
             achievement = new List<bool>();
@@ -48,20 +60,32 @@ public class AchievementController : MonoBehaviour {
             FileStream file = File.Create(Application.persistentDataPath + "/achievement.gd");
             bf.Serialize(file, achievement);
             file.Close();
+
+            foreach (GameObject image in lockedImage)
+            {
+                //game object are named in "1 Locked", "2 Locked". split the string to get the value
+                image.SetActive(true);  
+
+            }
+
+            foreach (GameObject image in unlockedImage)
+            {
+                image.SetActive(false);
+            }
         }
 	}
 	
 
-    public void unlockAchievement(int type)
+    public void setAchievement(int type)
     {
         achievement.RemoveAt(type);
         achievement.Insert(type, true);
 
-        GameObject[] lockedImage = GameObject.FindGameObjectsWithTag("LockedAchievement");
-        GameObject[] unlockedImage = GameObject.FindGameObjectsWithTag("UnlockedAchievement");
 
+        //find the locked image for the targeted achievement and disable it
         foreach (GameObject image in lockedImage)
         {
+            print(image.name);
             int count = type + 1;
             string name = count + " Locked";
             
@@ -72,10 +96,11 @@ public class AchievementController : MonoBehaviour {
             }
         }
 
+        //find the unlocked image for the targeted achievement and enable it
         foreach (GameObject image in unlockedImage) {
             int count = type + 1;
             string name = count + " Unlocked";
-
+            print(image.name);
             if (image.name.Equals(name))
             {
                 image.SetActive(true);
