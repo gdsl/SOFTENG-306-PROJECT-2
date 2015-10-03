e64429cffd967bb4e45ae6944e1eaa7243409897
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-03 17:55:24 +1300

    add in key status into gui

--- Assets/Scripts/GuiUpdate.cs
+++ Assets/Scripts/GuiUpdate.cs
@@ -4,11 +4,21 @@ using UnityEngine.UI;
 
 public class GuiUpdate : MonoBehaviour {
     public Text cookieText;
-	
+    public Text keyText;
+
 	// Update is called once per frame
 	void Update () {
         //show cookie count
         PlayerInventory playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
         cookieText.text = "Cookies: " + playerInventory.getCookieCount();
+        if (playerInventory.hasKey(1))
+        {
+            keyText.text = "Key: Have Key";
+        }
+        else
+        {
+            keyText.text = "Key: Need Key";
+        }
+
 	}
 }
