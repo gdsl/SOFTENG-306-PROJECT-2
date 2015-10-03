7741d53c1faf44633fed51297948d2dfe77bc683
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-03 17:34:13 +1300

    add key rotation and fix up cookie display problem

--- Assets/Scripts/CookieScript.cs
+++ Assets/Scripts/CookieScript.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
+
 
 public class CookieScript : Items {
-
-	public Text cookieText;
-
     //when the player collider with cookie trigger this event
     void OnTriggerEnter(Collider col)
     {
@@ -13,7 +10,6 @@ public class CookieScript : Items {
         {
             this.setPickedUp();//set cookie to pickup
             this.getPlayerInventory().IncreaseCookieCount();//increase cookie count
-			cookieText.text = "Cookies: " + this.getPlayerInventory().getCookieCount();
         }
     }
 }
---
+++ Assets/Scripts/GuiUpdate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GuiUpdate : MonoBehaviour {
+    public Text cookieText;
+	
+	// Update is called once per frame
+	void Update () {
+        //show cookie count
+        PlayerInventory playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+        cookieText.text = "Cookies: " + playerInventory.getCookieCount();
+	}
+}
