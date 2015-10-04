b4a10b8faacfd95f559bc0e968913afaa7fe0143
Chuan-Yu Wu <chuan1994@hotmail.com>
2015-10-04 21:25:37 +1300

    Word design polishes. Made final spot more clear

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        float f = Random.value + 0.5f;
+        this.gameObject.GetComponent<Light>().intensity = f;
+	}
+}
