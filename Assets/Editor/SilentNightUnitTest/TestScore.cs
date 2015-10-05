f13ca3837551121a0aebc31d7e4d26eaf89b2ba3
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-05 21:49:29 +1300

    add more test for score

--- Assets/Editor/SilentNightUnitTest/TestScore.cs
+++ Assets/Editor/SilentNightUnitTest/TestScore.cs
@@ -109,12 +109,83 @@ public class TestScore
         Slider suspicionSlider = gb.AddComponent<Slider>();
         st.text = "Time: 0";
         ct.text = "Cookie: 0";
-        suspicionSlider.value = 100f;
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 5000f;
         le.cookieText = ct;
         le.timeText = st;
         le.suspicionSlider = suspicionSlider;
         le.CalculateScore();
         Assert.AreEqual(0, le.getScore());
+    }
+
+    /**
+     * Test over max slider increment values effect on score should be cap by max
+     */
+    [Test]
+    public void testSliderOverMax()
+    {
+        LevelEnd le = new LevelEnd();
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 0";
+        ct.text = "Cookie: 0";
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 6000f;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(0, le.getScore());
+    }
+
+    /**
+     * Test combination of the effect of all 3 factor (time cookie slider) on score
+     */
+    [Test]
+    public void testCombineFactors()
+    {
+        LevelEnd le = new LevelEnd();
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 100";
+        ct.text = "Cookie: 1";
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 2000f;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(3400, le.getScore());
+        Assert.AreEqual(3, le.getStar());
+    }
+
+    /**
+     * Test under max slider increment values effect on score should the slider value
+     */
+    [Test]
+    public void testSliderUnderMax()
+    {
+        LevelEnd le = new LevelEnd();
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 0";
+        ct.text = "Cookie: 0";
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 3000f;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(2000, le.getScore());
     }
 
     /**
