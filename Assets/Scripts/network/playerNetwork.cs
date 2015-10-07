3a5ff3f75477f2de8be83358694f52bbf4a6f934
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-07 22:25:40 +1300

    attempt networking feature currently using local host

---
+++ Assets/Scripts/network/playerNetwork.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playerNetwork : NetworkBehaviour {
+
+    [SerializeField]Camera isoCamera;
+	// Use this for initialization
+	void Start () {
+	    if(isLocalPlayer){
+            GameObject.Find("Main Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            
+        }
+	}
+}
