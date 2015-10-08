ac0a2c0c69b0df40eaf043e9b5c48fff3dcd7590
unknown <jkim506@aucklanduni.ac.nz>
2015-10-08 14:24:28 +1300

    Configured page transitions with new level select and world select

--- Assets/UIDesign/Loading_Screen/Scripts/AsyncLoader.cs
+++ Assets/UIDesign/Loading_Screen/Scripts/AsyncLoader.cs
@@ -12,7 +12,7 @@ public class AsyncLoader : MonoBehaviour {
      // Use this for initialization
      void Start()
     {
-        StartCoroutine(DisplayLoadingScreen(3));
+        StartCoroutine(DisplayLoadingScreen(4));
     }
      
      // Update is called once per frame
