9db9daeb6ec21aa584aff2cafdbaa8400bca8b2d
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-08 20:03:44 +1300

    network scene test

---
+++ Assets/Scripts/network/playerMovementSync.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playerMovementSync : NetworkBehaviour {
+
+    [SyncVar] //server automatically apply to all client
+    private Vector3 pos;
+
+    [SerializeField]Transform myTransform;
+    [SerializeField]float lerpRate = 15;
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, pos,Time.deltaTime*lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos)
+    {
+        this.pos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            CmdProvidePositionToServer(myTransform.position);
+
+        }
+    }
+}
