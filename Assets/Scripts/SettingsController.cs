0b497a5ff9ca301802ee9264169fc10158615742
Wal8800 <johnlaw1234@hotmail.com>
2015-10-12 07:52:41 +1300

    created reset option

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -7,12 +7,14 @@ public class SettingsController : MonoBehaviour {
 	public Slider musicVolumeSlider;
 	public Slider soundEffectsVolumeSlider;
 	public InputField nameInput;
+    public GameObject confirmationPanel;
 
 	// Use this for initialization
 	void Start () {
 		musicVolumeSlider.value = PlayerPrefs.GetInt("musicVolume");
 		soundEffectsVolumeSlider.value = PlayerPrefs.GetInt("soundEffectsVolume");
 		nameInput.text = PlayerPrefs.GetString ("Name");
+        confirmationPanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -32,4 +34,22 @@ public class SettingsController : MonoBehaviour {
 		PlayerPrefs.SetString ("Name", nameInput.text);
 	}
 
+    public void resetData()
+    {
+        string name = PlayerPrefs.GetString("Name");
+        PlayerPrefs.DeleteAll();
+        musicVolumeSlider.value = musicVolumeSlider.maxValue/2;
+        soundEffectsVolumeSlider.value = soundEffectsVolumeSlider.maxValue / 2;
+        PlayerPrefs.SetString("Name", name);
+    }
+
+    public void showConfirmation()
+    {
+        confirmationPanel.SetActive(true);
+    }
+
+    public void hideConfirmation()
+    {
+        confirmationPanel.SetActive(false);
+    }
 }
