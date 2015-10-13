d62ec34e8cff49f8d63e87f471e3e3d7f829e531
unknown <jkim506@aucklanduni.ac.nz>
2015-10-13 13:27:50 +1300

    Commit to change branch

--- Assets/Scripts/UIDesign/Level_Select/Scripts/LevelStatusController.cs
+++ Assets/Scripts/UIDesign/Level_Select/Scripts/LevelStatusController.cs
@@ -17,6 +17,9 @@ public class LevelStatusController : MonoBehaviour {
     public Text levelTwoHighScore;
     public Text levelThreeHighScore;
 
+    private Color starCompleteColor = Color.yellow;
+    private Color starEmptyColor = Color.white;
+
     // Use this for initialization
     void Start () {
         //to be refractored later
@@ -27,9 +30,12 @@ public class LevelStatusController : MonoBehaviour {
 
     private void doSetup()
     {
-        Color starCompleteColor = Color.yellow;
-        Color starEmptyColor = Color.white;
+        setPageLevelOne();
+        setPageLevelTwo();
+        setPageLevelThree();
+    }
 
+    private void setPageLevelOne() {
         int levelOneStarsInt = PlayerPrefs.GetInt("Level One Stars");
 
         levelTwoStars[0].enabled = false;
@@ -52,9 +58,38 @@ public class LevelStatusController : MonoBehaviour {
         levelOneHighScore.text = PlayerPrefs.GetInt("Level One Score") + "";
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    private void setPageLevelTwo() {
+        
+
+        int levelTwoStarsInt = PlayerPrefs.GetInt("Level Two Stars");
+
+        if (levelTwoStarsInt > 0)
+        {
+            levelTwoStars[0].color = starCompleteColor;
+            if (levelTwoStarsInt > 1)
+            {
+                levelOneStars[1].color = starCompleteColor;
+                if (levelTwoStarsInt > 2)
+                {
+                    levelTwoStars[2].color = starCompleteColor;
+                }
+            }
+        }
+
+        levelTwoHighScore.text = PlayerPrefs.GetInt("Level Two Score") + "";
+
+    }
+
+
+    private void setPageLevelThree() {
+
+
+        levelThreeHighScore.text = PlayerPrefs.GetInt("Level Three Score") + "";
+    }
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
