5f41154f089411057fe59b02f7cc53846cafede1
Chang Kon Han <chang_kon@hotmail.com>
2015-10-15 02:10:00 +1300

    added michael/carl sitting prefab. added to level2 scene

---
+++ Assets/Scripts/SittingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SittingScript : MonoBehaviour {
+
+    public bool sitting;
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start ()
+    {
+        sitting = true;
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anim.SetBool("Sitting", sitting);
+    }
+}
