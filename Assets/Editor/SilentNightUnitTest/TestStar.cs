259bec100aa3f2bfd18fac9f0a7a4d1175982014
Yeu-Shin (Guyver) Fu <yfu959@aucklanduni.ac.nz>
2015-10-18 12:01:36 +1300

    update unit test to match new scoring mechanism

--- Assets/Editor/SilentNightUnitTest/TestStar.cs
+++ Assets/Editor/SilentNightUnitTest/TestStar.cs
@@ -4,148 +4,13 @@ using System.Collections;
 using UnityEngine.UI;
 using NUnit.Framework;
 
-public class TestScore
+public class TestStar
 {
-
     /**
-     * Test to if the base score is 5000
+     * Test to see if score is 2500 it is still 1 star for level 1
      */
     [Test]
-    public void testBaseScore()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st =gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct =gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 0";
-        ct.text = "Cookie: 0";
-        suspicionSlider.value = 0;
-        le.cookieText=ct;
-        le.timeText=st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(6000,le.getScore());
-    }
-
-    /**
-     * Test to see if the get a cookie increase point by 500
-     */
-    [Test]
-    public void testCookieScore()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 0";
-        ct.text = "Cookie: 1";
-        suspicionSlider.value = 0;
-        le.cookieText = ct;
-        le.timeText = st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(6500, le.getScore());
-    }
-
-    /**
-     * Test to see if the time decrease score by its amount
-     */
-    [Test]
-    public void testTimeScore()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 60";
-        ct.text = "Cookie: 0";
-        suspicionSlider.value = 0;
-        le.cookieText = ct;
-        le.timeText = st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(4200, le.getScore());
-    }
-
-    /**
-    * Test base slider increment values
-    */
-    [Test]
-    public void testSlider()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 0";
-        ct.text = "Cookie: 0";
-        suspicionSlider.value = 2f;
-        le.cookieText = ct;
-        le.timeText = st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(5999, le.getScore());
-    }
-
-    /**
-    * Test max slider increment values effect on score
-    */
-    [Test]
-    public void testSliderMax()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 0";
-        ct.text = "Cookie: 0";
-        suspicionSlider.maxValue = 5000f;
-        suspicionSlider.value = 5000f;
-        le.cookieText = ct;
-        le.timeText = st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(3500, le.getScore());
-    }
-
-    /**
-     * Test over max slider increment values effect on score should be cap by max
-     */
-    [Test]
-    public void testSliderOverMax()
-    {
-        LevelEnd le = new LevelEnd();
-        GameObject gb = new GameObject("stest");
-        GameObject gb2 = new GameObject("ctest");
-        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
-        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
-        Slider suspicionSlider = gb.AddComponent<Slider>();
-        st.text = "Time: 0";
-        ct.text = "Cookie: 0";
-        suspicionSlider.maxValue = 5000f;
-        suspicionSlider.value = 6000f;
-        le.cookieText = ct;
-        le.timeText = st;
-        le.suspicionSlider = suspicionSlider;
-        le.CalculateScore();
-        Assert.AreEqual(3500, le.getScore());
-    }
-
-    /**
-     * Test combination of the effect of all 3 factor (time cookie slider) on score
-     */
-    [Test]
-    public void testCombineFactors()
+    public void testBoundryOneStarLevelOne()
     {
         LevelEnd le = new LevelEnd();
         le.threeStarScore = 5000;
@@ -155,21 +20,47 @@ public class TestScore
         UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
         Slider suspicionSlider = gb.AddComponent<Slider>();
         st.text = "Time: 100";
-        ct.text = "Cookie: 1";
+        ct.text = "Cookie: 0";
         suspicionSlider.maxValue = 5000f;
-        suspicionSlider.value = 2000f;
+        suspicionSlider.value = 1000f;
         le.cookieText = ct;
         le.timeText = st;
         le.suspicionSlider = suspicionSlider;
         le.CalculateScore();
         Assert.AreEqual(2500, le.getScore());
+        Assert.AreEqual(1, le.getStar());
     }
 
     /**
-     * Test under max slider increment values effect on score should the slider value
+     * Test to see if score is less than 2500 it is 1 star for level 1
      */
     [Test]
-    public void testSliderUnderMax()
+    public void testOneStarLevelOne()
+    {
+        LevelEnd le = new LevelEnd();
+        le.threeStarScore = 5000;
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 120";
+        ct.text = "Cookie: 0";
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 0f;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(2400, le.getScore());
+        Assert.AreEqual(1, le.getStar());
+    }
+
+    /**
+    * Test to see if score is 5000 it is still 2 star for level 1
+    */
+    [Test]
+    public void testBoundryTwoStarLevelOne()
     {
         LevelEnd le = new LevelEnd();
         le.threeStarScore = 5000;
@@ -181,11 +72,85 @@ public class TestScore
         st.text = "Time: 0";
         ct.text = "Cookie: 0";
         suspicionSlider.maxValue = 5000f;
-        suspicionSlider.value = 3000f;
+        suspicionSlider.value = 2000f;
         le.cookieText = ct;
         le.timeText = st;
         le.suspicionSlider = suspicionSlider;
         le.CalculateScore();
-        Assert.AreEqual(4500, le.getScore());
+        Assert.AreEqual(5000, le.getScore());
+        Assert.AreEqual(2, le.getStar());
+    }
+
+    /**
+     * Test to see if score is 2500-5000 (exclusive) it is 2 star for level 1
+     */
+    [Test]
+    public void testTwostarLevelOne()
+    {
+        LevelEnd le = new LevelEnd();
+        le.threeStarScore = 5000;
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 100";
+        ct.text = "Cookie: 0";
+        suspicionSlider.value = 0;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(3000, le.getScore());
+        Assert.AreEqual(2, le.getStar());
+    }
+
+    /**
+   * Test to see if score is 5001 it is still 3 star for level 1
+   */
+    [Test]
+    public void testBoundryThreeStarLevelOne()
+    {
+        LevelEnd le = new LevelEnd();
+        le.threeStarScore = 5000;
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 30";
+        ct.text = "Cookie: 0";
+        suspicionSlider.maxValue = 5000f;
+        suspicionSlider.value = 198f;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(5001, le.getScore());
+        Assert.AreEqual(3, le.getStar());
+    }
+
+    /**
+     * Test to see if score is >5001 (exclusive) it is 3 star for level 1
+     */
+    [Test]
+    public void testThreeStarLevelOne()
+    {
+        LevelEnd le = new LevelEnd();
+        le.threeStarScore = 5000;
+        GameObject gb = new GameObject("stest");
+        GameObject gb2 = new GameObject("ctest");
+        UnityEngine.UI.Text st = gb.AddComponent<UnityEngine.UI.Text>();
+        UnityEngine.UI.Text ct = gb2.AddComponent<UnityEngine.UI.Text>();
+        Slider suspicionSlider = gb.AddComponent<Slider>();
+        st.text = "Time: 10";
+        ct.text = "Cookie: 1";
+        suspicionSlider.value = 0;
+        le.cookieText = ct;
+        le.timeText = st;
+        le.suspicionSlider = suspicionSlider;
+        le.CalculateScore();
+        Assert.AreEqual(6200, le.getScore());
+        Assert.AreEqual(3, le.getStar());
     }
 }
