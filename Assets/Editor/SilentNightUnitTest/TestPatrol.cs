f898d974f475721a4e2774124d7c79503cd7e807
Jared <jyou338@gmail.com>
2015-10-18 23:03:44 +1300

    Added unit test for patrol AI

--- Assets/Editor/SilentNightUnitTest/TestPatrol.cs
+++ Assets/Editor/SilentNightUnitTest/TestPatrol.cs
@@ -4,21 +4,21 @@ using System.Collections;
 using UnityEngine.UI;
 using NUnit.Framework;
 
-public class TestSleepWalker
+public class TestPatrol
 {
     /**
-    * Test to check that the sleepwalker is woken correctly
+    * Test to check that the patrol AI is woken correctly
     */
     [Test]
     public void TestWoken()
     {
-        GameObject SleepWalker = GameObject.Instantiate(Resources.Load("Victoria")) as GameObject;
+        GameObject SleepWalker = GameObject.Instantiate(Resources.Load("Michael")) as GameObject;
         GameObject touchObject = SleepWalker.transform.FindChild("SenseTouch").gameObject;
         Woken woken = touchObject.GetComponent<Woken>();
         SleepWalkerAI sleepwalkerAI = SleepWalker.GetComponent<SleepWalkerAI>();
 
         GameObject santa = GameObject.Instantiate(Resources.Load("QueryChan")) as GameObject;
-        
+
         Assert.That(woken.woken == false);
 
         woken.OnTriggerStay(santa.GetComponent<CapsuleCollider>()); // Unsure if this is proper practice as we have to make the ontrigger function public
